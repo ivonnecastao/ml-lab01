de5c6ce0acc8165f05982fd16ff0376db2e40323
Ivonne Castaño Osorio <ivonnecastao@utp.edu.co>
2023-05-18 14:42:06 -0500

    Mayo 18 2:41

--- code/Ejercicio_numpy.py
+++ code/Ejercicio_numpy.py
@@ -1,58 +1,100 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+
+# PREPARACIÓN DE DATOS
+
+#CARGUE
+print("DATOS INICIALES:\n")
 
 df = pd.read_csv('data\downloaded\winequality-white.csv', sep=";")
-print(df)
-print(df.shape) # me dice las dimensiones del dataframe
+print(f"Dataframe:\n {df}")
+print(f"Dim Dataframe = {df.shape}")  # me dice las dimensiones del dataframe
 
-df = df.dropna()
-print(df)
+#LIMPIEZA
+
+df = df.dropna() # Elimino datos incompletos. Limpio datos
+print(f"Dim Dataframe limpio = {df.shape}")
+
+# TRANSFORMACIÓN
 
 mnp = df.to_numpy()  # me convierte el df en una matriz de numpy (mnp)
-print(mnp)
-print(mnp.shape)  # me dice las dimensiones de la mnp
+#print(mnp)
+#print(mnp.shape)  # me dice las dimensiones de la mnp
 
 i = 11 # columna que queremos obtener
 y = np.array([mnp[:,i]]) # Extrae la columna i y la guarda en y
-print(y)
-print(y.shape)
+#print(y)
+#print(y.shape)
 
-y = np.transpose(y)
-print(y)
-print(y.shape)
+y = np.transpose(y) # Transpongo y porque quedó como fila y necesito columna
+#print(y)
+#print(y.shape)
 
 mnp = np.delete(mnp, 11, axis=1) # Elimino la columna que extraje en y
-print(mnp)
-print(mnp.shape)
+#print(mnp)
+#print(mnp.shape)
 
 vones= np.ones((y.shape[0],1)) # Creo el vector de 1's BIAS
-print(vones)
-print(vones.shape)
+#print(vones)
+#print(vones.shape)
 
-mnp = np.hstack([vones, mnp])
-print(mnp)
-print(mnp.shape)
+mnp = np.hstack([vones, mnp]) # Integro el BIAS al mnp
+#print(mnp)
+#print(mnp.shape)
 
-Theta_best = np.linalg.inv(mnp.T @ mnp) @ (mnp.T @ y)  #y= x Theta1 + Theta 0
+#DATOS PREPARADOS
+print(f"Matriz mnp:\n {mnp}")
+print(f"Dim Matriz mnp = {mnp.shape}")
+print(f"Vector y:\n {y}")
+print(f"Dim Vector y = {y.shape}")
+
+
+# MODELO
+
+Theta_best = np.linalg.inv(mnp.T @ mnp) @ (mnp.T @ y)  # y= Theta 0 + x Theta 1 
 print(f"Theta_best = {Theta_best}")
-print(Theta_best.shape)
-
-y_pred = mnp @ Theta_best  #resultado, predicción
-print(y_pred)
-print(y_pred.shape)
+print(f"Dim Theta_best = {Theta_best.shape}")
 
 
-"""
-# plot data
-plt.scatter(y, y_pred, label='Datos')
-#plt.plot(y, y_pred, color='red', label='Ajuste lineal') 
-plt.xlabel('Variables independiente (mnp)')
-plt.ylabel('Variable dependiente (y)')
-plt.legend()
-plt.title('Regresión Lineal con Numpy')
+y_pred = mnp @ Theta_best  # resultado, predicción
+print(f"Vector y_pred = {y_pred}")
+print(f"Dim Vector y_pred = {y_pred.shape}")
+
+
+# EVALUACIÓN DEL MODELO
+
+print("\nEVALUACIÓN DEL MODELO DE REGRESIÓN LINEAL CON NUMPY:\n")
+
+# Cálculo del error MSE
+mse = mean_squared_error(y, y_pred)
+# Calculo del error RMSE
+rmse = np.sqrt(np.mean((y - y_pred)**2))  # y_test - y_pred es el residual
+# Calculo del error MAE
+mae = np.mean(np.abs(y - y_pred))
+
+print(f"MSE: {mse}")
+print(f'RMSE: {rmse:.4f}')
+print(f'MAE: {mae:.4f}')
+
+
+# VISUALIZACIÓN
+
+# Se visualiza que tan bueno es el modelo
+plt.scatter(y, y_pred) # Crear una nueva grafica de y_test vs y_pred
+#plt.plot(y_test, y_pred, color='red') 
+plt.xlabel('y')
+plt.ylabel('y_pred')
+plt.title("Regresion lineal con sklearn: Qué tan bueno es el Modelo?")
+plt.savefig('plots\ Numpy y vs y_pred.png')
 plt.show()
-plt.savefig('plots\y_vs_y_pred_numpy.png')
-"""
 
-
+# Se visualiza el error de los datos
+plt.scatter(y, (y_pred - y))  # y_test - y_pred es el residual
+#plt.plot(y_test, y_pred, color='red')
+plt.xlabel('y')
+plt.ylabel('y_pred')
+plt.title("Regresion lineal con sklearn: Cuál es el error del Modelo?")
+plt.savefig('plots\ Numpy y vs y_pred-y_test.png')
+plt.show()
--- code/Ejercicio_sklearn.py
+++ code/Ejercicio_sklearn.py
@@ -5,68 +5,114 @@ from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
-#PREPARACIÓN
+
+# PREPARACIÓN DE DATOS
+
+#CARGUE
+print("DATOS INICIALES:\n")
 
 df = pd.read_csv('data\downloaded\winequality-white.csv', sep=";")
-print(df)
-print(df.shape) # me dice las dimensiones del dataframe
+print(f"Dataframe:\n {df}")
+print(f"Dim Dataframe = {df.shape}")  # me dice las dimensiones del dataframe
 
-df = df.dropna()
-print(df)
+#LIMPIEZA
+
+df = df.dropna() # Elimino datos incompletos. Limpio datos
+print(f"Dim Dataframe limpio = {df.shape}")
+
+# TRANSFORMACIÓN
 
 mnp = df.to_numpy()  # me convierte el df en una matriz de numpy (mnp)
-print(mnp)
-print(mnp.shape)  # me dice las dimensiones de la mnp
+#print(mnp)
+#print(mnp.shape)  # me dice las dimensiones de la mnp
 
 i = 11 # columna que queremos obtener
 y = np.array([mnp[:,i]]) # Extrae la columna i y la guarda en y
-print(y)
-print(y.shape)
+#print(y)
+#print(y.shape)
 
-y = np.transpose(y)
-print(y)
-print(y.shape)
+y = np.transpose(y) # Transpongo y porque quedó como fila y necesito columna
+#print(y)
+#print(y.shape)
 
 mnp = np.delete(mnp, 11, axis=1) # Elimino la columna que extraje en y
-print(mnp)
-print(mnp.shape)
+#print(mnp)
+#print(mnp.shape)
 
 vones= np.ones((y.shape[0],1)) # Creo el vector de 1's BIAS
-print(vones)
-print(vones.shape)
+#print(vones)
+#print(vones.shape)
 
-mnp = np.hstack([vones, mnp])
-print(mnp)
-print(mnp.shape)
+mnp = np.hstack([vones, mnp]) # Integro el BIAS al mnp
+#print(mnp)
+#print(mnp.shape)
 
-#MODELO
+#DATOS PREPARADOS
+print(f"Matriz mnp:\n {mnp}")
+print(f"Dim Matriz mnp = {mnp.shape}")
+print(f"Vector y:\n {y}")
+print(f"Dim Vector y = {y.shape}")
+
+
+# MODELO
 
 mnp_train, mnp_test, y_train, y_test = train_test_split(mnp, y, test_size=0.2)
 
-print(f"mnp_train.shape = {mnp_train.shape}")
-print(f"mnp_test.shape = {mnp_test.shape}")
+print("\nMODELO:\n")
+print(f"Dim mnp_train = {mnp_train.shape}")
+print(f"Dim y_train = {y_train.shape}")
+print(f"Dim mnp_test = {mnp_test.shape}")
+print(f"Dim y_test = {y_test.shape}")
 
 model = LinearRegression()
-model.fit(mnp_train, y_train)
+model.fit(mnp_train, y_train)   # Este método se utiliza para entrenar los modelos con los datos de entrenamiento. 
+                                # Es decir, se utiliza para obtener los parámetros del modelo con los que se 
+                                # minimiza el error de predicción en los datos de entrenamiento. Debido a que se 
+                                # emplea para entrenar el modelo fit() se suele usar sobre los datos de entrenamiento.
 
-y_pred = model.predict(mnp_test)
-print(y_pred)
-print(y_pred.shape)
+y_pred = model.predict(mnp_test)    # Este método se utiliza para realizar obtener las predicciones de un modelo sobre 
+                                    # un conjunto de datos de entrada desconocidos. Es decir, una vez entrenado el modelo, 
+                                    # se puede utilizar el método predict() saber los valores o etiquetas que asigna el 
+                                    # modelo a una conjunto de datos. Dada la finalidad de este método, suele usarse 
+                                    # con los datos de test o cualquier conjunto desconocido.
+#print(f"Vector y_pred = {y_pred}")
+print(f"Dim Vector y_pred = {y_pred.shape}")
 
+
+# EVALUACIÓN DEL MODELO
+
+print("\nEVALUACIÓN DEL MODELO DE REGRESIÓN LINEAL CON SKLEARN:\n")
+
+# Cálculo del error MSE
 mse = mean_squared_error(y_test, y_pred)
+# Calculo del error RMSE
+rmse = np.sqrt(np.mean((y_test - y_pred)**2))  # y_test - y_pred es el residual
+# Calculo del error MAE
+mae = np.mean(np.abs(y_test - y_pred))
 
-print(f"Coeficiente: {model.coef_}")
+print(f"Coeficiente Theta_best: {model.coef_}")
 print(f"Intercepto: {model.intercept_}")
-print(f"Error cuadratico medio: {mse}")
+print(f"MSE: {mse}")
+print(f'RMSE: {rmse:.4f}')
+print(f'MAE: {mae:.4f}')
 
-"""
-# Plot data
-plt.scatter(y_pred, y, label="Datos")
-#plt.plot(mnp_test, y_predict, color="red", label="Ajuste lineal")
 
-plt.xlabel("Variable independiente (y_pred)")
-plt.ylabel("Variable dependiente(y)")
-plt.title("Regresion lineal")
-plt.legend()
+# VISUALIZACIÓN
+
+# Se visualiza que tan bueno es el modelo
+plt.scatter(y_test, y_pred) # Crear una nueva grafica de y_test vs y_pred
+#plt.plot(y_test, y_pred, color='red') 
+plt.xlabel('y_test')
+plt.ylabel('y_pred')
+plt.title("Regresion lineal con sklearn: Qué tan bueno es el Modelo?")
+plt.savefig('plots\ Sklearn y_test vs y_pred.png')
 plt.show()
-"""
+
+# Se visualiza el error de los datos
+plt.scatter(y_test, (y_pred - y_test))  # y_test - y_pred es el residual
+#plt.plot(y_test, y_pred, color='red')
+plt.xlabel('y_test')
+plt.ylabel('y_pred')
+plt.title("Regresion lineal con sklearn: Cuál es el error del Modelo?")
+plt.savefig('plots\ Sklearn y_test vs y_pred-y_test.png')
+plt.show()
