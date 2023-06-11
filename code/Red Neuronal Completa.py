c46eced2aeb0fddb409daea9f681988cc95a8b94
Ivonne Castaño Osorio <ivonnecastao@utp.edu.co>
2023-06-11 01:01:47 -0500

    Junio 11 1:00 am

--- code/Red Neuronal Completa.py
+++ code/Red Neuronal Completa.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.datasets import make_circles
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+import json
 
 #-------------------------------------------------------------------------------------------
 # FUNCIONES GENERALES
@@ -58,10 +59,10 @@ def matriz_confusion(y_pred, y):
     # Total Predicciones
     m = vp + fp + vn + fn
     
-    print(f"TOTAL: Instancias {m} / Instancias Verdaderas: {v} / Instancias Falsas: {f}")
+    #print(f"TOTAL: Instancias {m} / Instancias Verdaderas: {v} / Instancias Falsas: {f}")
     
-    print(f"VN: {vn}   FP: {fp}")
-    print(f"FN: {fn}   VP: {vp}")
+    #print(f"VN: {vn}   FP: {fp}")
+    #print(f"FN: {fn}   VP: {vp}")
 
     return vp, vn, fp, fn, m
 
@@ -75,17 +76,27 @@ def calculo_metricas(vp, vn, fp, fn, m):
     accuracy = (vn + vp) / m
     
     # Precision - Precisión
-    precision = vp / (vp + fp)
-    
+    if vp == 0 :
+        precision = 0
+    else:        
+        precision = vp / (vp + fp)
+        
     # Recall - Exhaustividad
-    recall = vp / (vp + fn)
+    if vp == 0 :
+        recall = 0
+    else:        
+        recall = vp / (vp + fn)
     
     # F1 Score - Puntaje F1
-    f1score = (2 * precision * recall) / (precision + recall)
+    if precision == 0 or recall == 0 :
+        f1score = 0
+    else:
+        f1score = (2 * precision * recall) / (precision + recall)
         
     return accuracy, precision, recall, f1score
 
-def curvas_roc_pr(y_pred, y):
+
+def curvas_roc_pr(y_pred, y, etapa):
     
     # Generar matriz ROC-PR para los gráficos de las Curvas ROC y PR
     roc_pr = np.zeros((9, 8))
@@ -96,13 +107,13 @@ def curvas_roc_pr(y_pred, y):
     while i < roc_pr.shape[0]:
         umbral = np.round(umbral + paso, 1)
         roc_pr[i, 0] = umbral
-        print(f"Umbral: {umbral} ->: ")
+        #print(f"Umbral: {umbral} ->: ")
         y_new = sintonizar_umbral(y_pred, umbral)
         roc_pr[i, 1], roc_pr[i, 2], roc_pr[i, 3], roc_pr[i, 4], roc_pr[i, 5] = matriz_confusion(y_new, y)
         
         if roc_pr[i, 1] == 0:
-            roc_pr[i, 6] = 0
-            roc_pr[i, 7] = 0
+            roc_pr[i, 6] = 0  # Precision
+            roc_pr[i, 7] = 0  # Recall
         else:
             roc_pr[i, 6] = roc_pr[i, 1] / (roc_pr[i, 1] + roc_pr[i, 3]) # Precision
             roc_pr[i, 7] = roc_pr[i, 1] / (roc_pr[i, 1] + roc_pr[i, 4]) # Recall
@@ -117,11 +128,12 @@ def curvas_roc_pr(y_pred, y):
     plt.xlabel('Tasa FP')
     plt.ylabel('Tasa VP')
     plt.xlim(-0.5, roc_pr[:, 3].max() + 5)  # Establece los límites del eje x 
-    plt.ylim(-0.5, roc_pr[:, 1].max() + 5)
+    plt.ylim(-0.5, roc_pr[:, 1].max() + 5)  # Establece los límites del eje y
     for j, label in enumerate(roc_pr[:, 0]):
         plt.text(roc_pr[j, 3], roc_pr[j, 1], label)
-    plt.title("Curva ROC")
-    plt.savefig('plots\Curva ROC.png')
+    plt.title(f"Curva ROC {etapa}")
+    ruta = "plots\Curva ROC " + etapa + ".png"
+    plt.savefig(ruta)
     plt.show()
 
     # Plot Curva PR
@@ -130,13 +142,75 @@ def curvas_roc_pr(y_pred, y):
     plt.plot(roc_pr[:, 7], roc_pr[:, 6], color='red') 
     plt.xlabel('Recall / Exhaustividad')
     plt.ylabel('Precision / Precisión')
-    plt.xlim(roc_pr[:, 7].min() - 1, roc_pr[:, 7].max() + 1)  # Establece los límites del eje x 
-    plt.ylim(roc_pr[:, 6].min() - 1, roc_pr[:, 6].max() + 1)
+    plt.xlim(roc_pr[:, 7].min() - 0.2, roc_pr[:, 7].max() + 0.5)  # Establece los límites del eje x 
+    plt.ylim(roc_pr[:, 6].min() - 0.2, roc_pr[:, 6].max() + 0.5)  # Establece los límites del eje y
     for j, label in enumerate(roc_pr[:, 0]):
         plt.text(roc_pr[j, 7], roc_pr[j, 6], label)
-    plt.title("Curva PR")
-    plt.savefig('plots\Curva PR.png')
+    plt.title(f"Curva PR {etapa}")
+    ruta = "plots\Curva PR " + etapa + ".png"
+    plt.savefig(ruta)
     plt.show()
+    
+    
+def mejor_modelo(archivo_pesos):
+    
+    # Cargar archivo
+    with open(archivo_pesos, 'r') as archivo:
+        pesos_leidos = json.load(archivo)
+       
+    # Buscar el mejor loss
+    for registro in pesos_leidos:
+        loss_ant = registro['loss']
+        break
+     
+    for registro in pesos_leidos:
+        loss_new = registro['loss']
+        if loss_new > loss_ant :
+            break
+        else:
+            loss_ant = registro['loss']
+        
+    mejor_loss = loss_ant
+     
+    # Buscar el mejor modelo
+    pesos_leidos = None
+        
+    with open(archivo_pesos, 'r') as archivo:
+        pesos_leidos = json.load(archivo)
+    
+    for registro in pesos_leidos:
+        if registro['loss'] == mejor_loss:
+            registro_mejor_modelo = registro
+            break
+        
+    # Extraer las variables del mejor modelo
+    epoch = registro_mejor_modelo['epoch']
+    W1 = np.array(registro_mejor_modelo['W1'])
+    W2 = np.array(registro_mejor_modelo['W2'])
+    loss = registro_mejor_modelo['loss']
+        
+    return epoch, W1, W2, loss
+
+def guardar_modelo(epoch, W1, W2, loss):
+        
+        archivo_modelo = 'model\modelo.json'
+        # Convertir W1 y W2 en listas
+        W1_lista = W1.tolist()
+        W2_lista = W2.tolist()
+
+        # Crear un diccionario con las variables
+        modelo = {
+            'epoch' : epoch,
+            'W1' : W1_lista,
+            'W2' : W2_lista,
+            'loss' : loss
+        }
+
+        # Guardar el diccionario en un archivo JSON
+        with open(archivo_modelo, 'w') as archivo:
+            json.dump([modelo], archivo)
+
+
     
 #-------------------------------------------------------------------------------------------
 # ARQUITECTURA DE LA RED NEURONAL
@@ -147,7 +221,50 @@ class SimpleNN:
         self.b1 = np.zeros((1, num_hidden))
         self.W2 = np.random.randn(num_hidden, num_outputs)
         self.b2 = np.zeros((1, num_outputs))
+        
 
+    def iniciar_archivo_pesos(self, archivo_pesos, epoch, loss):
+        # Convertir self.W1 y self.W2 en listas
+        W1_lista = self.W1.tolist()
+        W2_lista = self.W2.tolist()
+
+        # Crear un diccionario con las variables
+        pesos = {
+            'epoch' : epoch,
+            'W1' : W1_lista,
+            'W2' : W2_lista,
+            'loss' : loss
+        }
+
+        # Guardar el diccionario en un archivo JSON
+        with open(archivo_pesos, 'w') as archivo:
+            json.dump([pesos], archivo)
+    
+    
+    def guardar_pesos(self, archivo_pesos, epoch, loss):
+        # Convertir self.W1 y self.W2 en listas
+        W1_lista = self.W1.tolist()
+        W2_lista = self.W2.tolist()
+
+        # Crear un diccionario con las variables
+        pesos = {
+            'epoch' : epoch,
+            'W1' : W1_lista,
+            'W2' : W2_lista,
+            'loss' : loss
+        }
+
+        # Leer los datos existentes del archivo JSON
+        with open(archivo_pesos, 'r') as archivo:
+            pesos_existentes = json.load(archivo)
+        
+        pesos_existentes.append(pesos)
+                
+        # Guardar el diccionario en un archivo JSON
+        with open(archivo_pesos, 'w') as archivo:
+            json.dump(pesos_existentes, archivo)
+            
+       
     def binary_cross_entropy(self, y, y_hat):
         m = y.shape[0]
         bce = -1 / m * np.sum(y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat))
@@ -183,8 +300,8 @@ class SimpleNN:
         self.b1 -= lr * db1
         self.W2 -= lr * dW2
         self.b2 -= lr * db2
-
-    def train(self, X, y, epochs, lr):
+        
+    def train(self, X, y, epochs, lr, archivo):
         
         mem = np.zeros((int(epochs/10000), 2)) # Almacena el histórico de loss x epochs
         i = 0
@@ -204,13 +321,18 @@ class SimpleNN:
         plt.ylabel("$X_2$")
         plt.pause(0.1)
 
+        # Guardar pesos iniciales
+        self.iniciar_archivo_pesos(archivo, -1, epochs)
+        
         # Entrenamiento
         for epoch in range(epochs):
             z1, a1, z2, y_hat = self.feedforward(X)
             dW1, db1, dW2, db2 = self.backpropagation(X, y, z1, a1, z2, y_hat)
             self.update_parameters(dW1, db1, dW2, db2, lr)
+            
             if epoch % 10000 == 0:
                 loss = self.binary_cross_entropy(y, y_hat)
+                self.guardar_pesos(archivo, epoch, loss)
                 print(f"Epoch: {epoch}, loss: {loss}")
                 mem[i, :] = [epoch, loss]
                 i = i + 1
@@ -250,9 +372,23 @@ class SimpleNN:
         _, _, _, y_hat = self.feedforward(X)
         return np.round(y_hat)
 
-
+    def feedforward_eval(self, X, W1, W2):
+        z1 = np.dot(X, W1) + self.b1
+        a1 = self.sigmoid(z1)
+        z2 = np.dot(a1, W2) + self.b2
+        y_hat = self.sigmoid(z2)
+        return y_hat
+    
+    def predict_eval(self, X, W1, W2):
+        y_hat = self.feedforward_eval(X, W1, W2)
+        return y_hat
+    
+    
 #-------------------------------------------------------------------------------------------
 # MAIN
+
+# Especificar la ruta y el nombre del archivo JSON donde se guardarán los pesos
+archivo_json = 'model\pesos.json'
 
 # Generación del dataset 
 np.random.seed(0)
@@ -263,9 +399,9 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 # Parámetros Iniciales
 num_inputs = X_train.shape[1]
-num_hidden = 50
+num_hidden = 92
 num_outputs = 1
-epochs = 1000000
+epochs = 50000
 lr = 0.02
 
 # Generación de la RN
@@ -276,7 +412,7 @@ nn = SimpleNN(num_inputs, num_hidden, num_outputs)
 print("ETAPA DE ENTRENAMIENTO DEL MODELO:")
 
 # Entrenamiento de la RN
-nn.train(X_train, y_train, epochs=epochs, lr=lr)
+nn.train(X_train, y_train, epochs=epochs, lr=lr, archivo = archivo_json)
 
 # Generación de Predicción
 y_pred_train = nn.predict(X_train)
@@ -287,6 +423,10 @@ y_pred_train = nn.predict(X_train)
 print("Matriz de Confusión - Entrenamiento ->")
 vp, vn, fp, fn, m = matriz_confusion(np.round(y_pred_train), y_train)
 
+print(f"VN: {vn}   FP: {fp}")
+print(f"FN: {fn}   VP: {vp}")
+print(f"TOTAL: Instancias {m}")
+
 print("Métricas Entrenamiento ->")
 train_accuracy, train_precision, train_recall, train_f1score = calculo_metricas(vp, vn, fp, fn, m)
 
@@ -295,20 +435,31 @@ print(f"Training Precision: {train_precision * 100:.2f}% de las instancias clasi
 print(f"Training Recall: {train_recall * 100:.2f}% de las instancias que realmente son positivas son correctas (En el Dataset)")
 print(f"Training F1 Score: {train_f1score:.2f} / 1 en Precisión y Exhaustividad")
 
-curvas_roc_pr(y_pred_train, y_train)
+curvas_roc_pr(y_pred_train, y_train, "Entrenamiento")
 
 
 #...........................................................
 #EVALUACIÓN DEL MODELO
 print("ETAPA DE EVALUACIÓN DEL MODELO:")
 
-# Evaluación
-y_pred_test = nn.predict(X_test)
+# Extraer el mejor modelo
+epoch, W1, W2, loss = mejor_modelo(archivo_json)
 
-# Métricas del evaluación
+print(f"Mejor Modelo: Epoch {epoch}")
+
+guardar_modelo(epoch, W1, W2, loss)
+
+# Evaluación del modelo
+y_pred_test = nn.predict_eval(X_test, W1, W2)
+
+# Métricas de la evaluación
 
 print("Matriz de Confusión - Evaluación ->")
 vp, vn, fp, fn, m = matriz_confusion(np.round(y_pred_test), y_test)
+
+print(f"VN: {vn}   FP: {fp}")
+print(f"FN: {fn}   VP: {vp}")
+print(f"TOTAL: Instancias {m}")
 
 print("Métricas Evaluación ->")
 test_accuracy, test_precision, test_recall, test_f1score = calculo_metricas(vp, vn, fp, fn, m)
@@ -318,6 +469,8 @@ print(f"Test Precision: {test_precision * 100:.2f}% de las instancias clasificad
 print(f"Test Recall: {test_recall * 100:.2f}% de las instancias que realmente son positivas son correctas (En el Dataset)")
 print(f"Test F1 Score: {test_f1score:.2f} / 1 en Precisión y Exhaustividad")
 
-curvas_roc_pr(y_pred_test, y_test)
+curvas_roc_pr(y_pred_test, y_test, "Evaluación")
+
+
 
 
---
+++ code/ejemplo matriz.py
@@ -0,0 +1,90 @@
+import numpy as np
+from sklearn.datasets import make_circles
+import json
+
+
+# Especificar la ruta y el nombre del archivo JSON donde se guardarán los datos
+archivo_json = 'pesos.json'
+
+# Generación del dataset 
+np.random.seed(0)
+X, B = make_circles(n_samples=5, noise=0.05)
+
+print(f"X:\n {X}")
+print(f"Dim de X: {X.shape}")
+print(f"B:\n {B.reshape(-1,1)}")
+print(f"Dim de B: {B.shape}")
+
+# Definir la matriz A y el vector 
+#X  = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])
+#W1 = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]) 
+#W2 = np.array([[1], [2], [3], [4], [5]]) 
+#B1 = np.array([[1, 1, 1, 1, 1]])   
+#B2 = np.array([[1]]) 
+
+W1 = np.random.randn(2, 5)
+W2 = np.random.randn(5, 1)
+B1 = np.ones((1, 5))
+B2 = np.ones((1, 1))
+
+# Obtener las dimensiones de la matriz y el vector
+print(f"Dim de X: {X.shape}")
+print(f"Dim de W1: {W1.shape}")
+print(f"Dim de B1: {B1.shape}")
+
+XW1 = np.dot(X, W1)
+print(f"X.W1: {XW1}")
+print(f"Dim de X.W1: {XW1.shape}")
+
+Z1 = XW1 + B1
+print(f"Z1:\n {Z1}")
+print(f"Dim de Z1: {Z1.shape}")
+
+A1 = 1 / (1 + np.exp(-Z1))
+print(f"A1:\n {A1}")
+print(f"Dim de A1: {A1.shape}")
+
+print(f"Dim de W2: {W2.shape}")
+print(f"Dim de B2: {B2.shape}")
+
+A1W2 = np.dot(A1, W2)
+print(f"A1.W2: {A1W2}")
+print(f"Dim de A1.W2: {A1W2.shape}")
+
+Z2 = A1W2 + B2
+print(f"Z2:\n {Z2}")
+print(f"Dim de Z2: {Z2.shape}")
+
+Y_hat = 1 / (1 + np.exp(-Z2))
+print(f"Y_hat:\n {Y_hat}")
+print(f"Y_hat: {Y_hat.shape}")
+
+
+# Convertir la matriz en una lista de Python
+W1_lista = W1.tolist()
+W2_lista = W2.tolist()
+
+# Crear un diccionario con las variables que deseas guardar
+pesos = {
+    'W1' : W1_lista,
+    'W2' : W2_lista
+}
+
+# Guardar el diccionario en un archivo JSON
+with open(archivo_json, 'w') as archivo:
+    json.dump(pesos, archivo)
+    
+
+# Leer los datos del archivo JSON
+with open(archivo_json, 'r') as archivo:
+    pesos_leidos = json.load(archivo)
+
+# Guardar las variables en otras variables
+v1 = np.array(pesos_leidos['W1'])
+v2 = np.array(pesos_leidos['W2'])
+
+print(f"W1_archivo: {v1}")
+print(f"W1: {W1}")
+print(f"W2_archivo: {v2}")
+print(f"W2: {W2}")
+
